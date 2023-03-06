d721c86447c301614493079350b9f99f1632874c
Rui Lopes <ruilopes@Ruis-Air.Home>
2023-03-06 19:21:50 +0000

    Creating dummy routes

---
+++ src/pages/clients/[id]/index.js
@@ -0,0 +1,7 @@
+export default function ClientProjectsPage() {
+  return (
+    <div>
+      <h1>The Projects of a Given Cliente</h1>
+    </div>
+  );
+}
---
+++ src/pages/portfolio/list.js
@@ -0,0 +1,9 @@
+export default function ListPage() {
+  return (
+    <>
+      <div>
+        <h1>This is the list of portfolios</h1>
+      </div>
+    </>
+  );
+}
