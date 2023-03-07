7e06d33fbbc9928a74c4296e961dddd50c5557fe
Rui Lopes <ruilopes@Ruis-Air.Home>
2023-03-07 23:33:35 +0000

    Creating navigation for the app.

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+export default function NorFoundPage() {
+  return (
+    <>
+      <h1>Custom Page Not Found</h1>
+    </>
+  );
+}
--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "Rui", name: "Rui" },
+    { id: "Max", name: "Maximilian" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              {/* This one way of specifying the pathname */}
+              {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+              {/* Next.js allows us to specfy links/path this way */}
+              <Link
+                href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+              >
+                {client.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
